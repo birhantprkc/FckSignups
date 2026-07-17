9f6ad66e549f3dcf5b7fbabe74345c459c25b4c1
Moamal-2000 <moamalalaapro1@gmail.com>
2026-07-18 02:14:08 +0300

    a11y(toast): enhance semantic structure and convert close icon to an interactive button

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -7,22 +7,22 @@ export function Toast({ innerText, onExit }: ToastProps) {
   return (
     <>
       <div className="toast-container">
-        <span>{innerText}</span>
-        <svg
-          onClick={onExit}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          style={{ minWidth: "1.6rem", maxWidth: "1.6rem" }}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18 18 6M6 6l12 12"
-          />
-        </svg>
+        <p>{innerText}</p>
+        <button type="button" onClick={onExit} className="close-toast-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </>
   );
